d2f843707f610a3ba8f55b4f33ff11e16005737d
babishkarki <babishkarki629@gmail.com>
2023-03-30 09:11:02 +0545

    Responsive error fixed

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function calculateResult(){
         result.value = 'Error'
     }
 }
+
+// Responsive
+var result = document.getElementById('result1');
+
+function calculation(value){
+    result.value = result.value + value;
+}
+
+function clearResult(){
+    result.value = '';
+}
+
+function calculateResult(){
+    try {
+        result.value = eval(result.value);
+    }
+    catch (error){
+        result.value = 'Error'
+    }
+}
