f090d485745254c43635fa547f9c13a812aa9f80
babishkarki <babishkarki629@gmail.com>
2023-03-30 08:25:43 +0545

    Responsive calculator

---
+++ script.js
@@ -0,0 +1,18 @@
+var result = document.getElementById('result');
+
+function appendToResult(value){
+    result.value = result.value + value;
+}
+
+function clearResult(){
+    result.value = '';
+}
+
+function calculateResult(){
+    try {
+        result.value = eval(result.value);
+    }
+    catch (error){
+        result.value = 'Error'
+    }
+}
