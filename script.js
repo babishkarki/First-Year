4892af59228c0000cfaf794610e986b385e389cf
babishkarki <babishkarki629@gmail.com>
2023-03-30 09:37:34 +0545

    Responsive fixed

--- script.js
+++ script.js
@@ -16,23 +16,3 @@ function calculateResult(){
         result.value = 'Error'
     }
 }
-
-// Responsive
-var result = document.getElementById('result1');
-
-function calculation(value){
-    result.value = result.value + value;
-}
-
-function clearResult(){
-    result.value = '';
-}
-
-function calculateResult(){
-    try {
-        result.value = eval(result.value);
-    }
-    catch (error){
-        result.value = 'Error'
-    }
-}
